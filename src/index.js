b61aa51963deb4e8c1511585e658b1fb2dc0fd99
DBDev-IT-2 <damir.b.dbdev@gmail.com>
2026-06-26 07:42:34 +0000

    index.js: Forgot to add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
 import "./server/auth.js";
 import "./server/projects.js";
-import "./server/featured-projects.js";
 import "./server/users.js";
 import "./server/admin.js";
+import "./server/featured-projects.js";
+import "./server/";
 
 // eslint-disable-next-line no-console
 app.listen(process.env.PORT, "127.0.0.1", () => console.log(`Port ${process.env.PORT}`));
