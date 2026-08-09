9079ab829a03de7ae589f211f1fadd79a77145b7
Damir <dambul2809@mail.ru>
2026-08-09 11:11:20 +0000

    image-processing.js: Fixes

--- src/server/image-processing.js
+++ src/server/image-processing.js
@@ -3,53 +3,50 @@ import sharp from "sharp";
 const AVATAR_SIZE = 64;
 const THUMBNAIL_WIDTH = 480;
 const THUMBNAIL_HEIGHT = 360;
+const MAX_INPUT_PIXELS = 50_000_000;
+const MAX_DIMENSION = 10000;
 const ALLOWED_IMAGE_FORMATS = new Set(["jpeg", "png", "webp", "gif", "svg", "avif", "heif"]);
 
-const validateImageBuffer = async (buffer) => {
-	try {
-		const metadata = await sharp(buffer).metadata();
-		return ALLOWED_IMAGE_FORMATS.has(metadata.format);
-	} catch (_) {
-		return false;
-	}
-};
+const validateImageMetadata = async (buffer) => {
+	const image = sharp(buffer, { limitInputPixels: MAX_INPUT_PIXELS });
+	const metadata = await image.metadata();
 
-const formatAvatarImage = async (buffer) => {
-	if (!(await validateImageBuffer(buffer))) {
+	if (!ALLOWED_IMAGE_FORMATS.has(metadata.format)) {
+		throw new Error("Unsupported image format");
+	}
+
+	if (!metadata.width || !metadata.height) {
 		throw new Error("Invalid image file");
 	}
 
-	return sharp(buffer)
+	if (metadata.width > MAX_DIMENSION || metadata.height > MAX_DIMENSION) {
+		throw new Error("Image dimensions too large");
+	}
+
+	if (metadata.width * metadata.height > MAX_INPUT_PIXELS) {
+		throw new Error("Image dimensions too large");
+	}
+
+	return image;
+};
+
+const createProcessedBuffer = async (buffer, width, height) => {
+	const image = await validateImageMetadata(buffer);
+	return image
 		.rotate()
-		.resize(AVATAR_SIZE, AVATAR_SIZE, {
+		.resize(width, height, {
 			fit: "cover",
-			position: "centre"
+			position: "centre",
+			fastShrinkOnLoad: true
 		})
 		.png({
 			compressionLevel: 9,
-			adaptiveFiltering: true,
-			quality: 80
+			adaptiveFiltering: true
 		})
 		.toBuffer();
 };
 
-const formatThumbnailImage = async (buffer) => {
-	if (!(await validateImageBuffer(buffer))) {
-		throw new Error("Invalid image file");
-	}
-
-	return sharp(buffer)
-		.rotate()
-		.resize(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, {
-			fit: "cover",
-			position: "centre"
-		})
-		.png({
-			compressionLevel: 9,
-			adaptiveFiltering: true,
-			quality: 80
-		})
-		.toBuffer();
-};
+const formatAvatarImage = async (buffer) => createProcessedBuffer(buffer, AVATAR_SIZE, AVATAR_SIZE);
+const formatThumbnailImage = async (buffer) => createProcessedBuffer(buffer, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
 
 export { formatAvatarImage, formatThumbnailImage };
